cec0cdd52b4a5bd949904c452917ae7e025aa44a
Git-Jocelio <trelicas.aricanga@gmail.com>
2026-03-24 10:43:30 -0300

    Delete Segundo Semestre/Disciplinas/Desenvolvimento Web II/atividade 4/app directory

--- Segundo Semestre/Disciplinas/Desenvolvimento Web II/atividade 4/app/src/models/interfaces/instances.ts
+++
@@ -1,7 +0,0 @@
-import { Stack } from "./stack";
-import { List } from "./List";
-import { Queue } from "./Queue";
-
-export const stack = new Stack<unknown>("Pilha Principal");
-export const list = new List<unknown>("Lista");
-export const queue = new Queue<unknown>("Queue");
